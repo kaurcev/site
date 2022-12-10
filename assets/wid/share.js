1b4d16d12bf98541c6bf6f29d44ebd3e651a5f01
Александр Каурцев <91284843+kaurcev@users.noreply.github.com>
2022-12-10 15:31:17 +0600

    Виджет для сайта "поделиться"

--- assets/wid/share.js
+++ assets/wid/share.js
@@ -1,5 +1,5 @@
 function load() {
-    document.getElementById("share").innerHTML = '<div id="qr_code"></div > ';
+    document.getElementById("share").innerHTML = '<div id="qr_code"></div> <p>Поделитесь страницей :)</p>';
     if (document.getElementById('qr_code') != undefined) {
         const el = (selector) => document.querySelector(selector);
         let qrcode = el('#qr_code');
