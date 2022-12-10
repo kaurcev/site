a24e8c96fdbb3845039f46c7bfed896329c3e4c5
Александр Каурцев <91284843+kaurcev@users.noreply.github.com>
2022-12-10 15:27:41 +0600

    Fixed

--- assets/wid/share.js
+++ assets/wid/share.js
@@ -1,8 +1,10 @@
-document.getElementById('share').innerHTML = '<div id="qr_code"></div > ';
-if (document.getElementById('qr_code') != undefined) {
-    const el = (selector) => document.querySelector(selector);
-    let qrcode = el('#qr_code');
-    let text = window.location.href;
-    qrcode.innerHTML = "";
-    qrcode.append(QRCode.generateHTML(text, {}))
+function load() {
+    document.getElementById("share").innerHTML = '<div id="qr_code"></div > ';
+    if (document.getElementById('qr_code') != undefined) {
+        const el = (selector) => document.querySelector(selector);
+        let qrcode = el('#qr_code');
+        let text = window.location.href;
+        qrcode.innerHTML = "";
+        qrcode.append(QRCode.generateHTML(text, {}))
+    }
 }
